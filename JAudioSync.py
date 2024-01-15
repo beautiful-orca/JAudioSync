b566d0d3bea44149653f61013b7b662948015f21
beautiful-orca <125277611+beautiful-orca@users.noreply.github.com>
2024-01-15 13:04:38 +0100

    cleanup and code comments

--- JAudioSync.py
+++ JAudioSync.py
@@ -12,10 +12,10 @@ from mutagen.mp3 import MP3
 from math import ceil
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+# generate time when playback should start if no parameter is given, 10-20 seconds ahead
 def get_next_time():    
     second = datetime.now().second
     minute = datetime.now().minute
-    
     if 0 <= second <= 10:
         next_time = datetime.now()
         next_time = next_time.replace(second=20,  microsecond=0).strftime('%H:%M:%S')
@@ -57,6 +57,7 @@ def string_to_datetime(time_string):
     dt = datetime.strptime(datetime_str, f"%Y-%m-%d {format_str}")
     return dt
 
+# read last saved playlist position, for resuming "-p res"
 def read_resume_position():
     try:
         with open('resume_pos.pkl', 'rb') as file:
@@ -66,7 +67,7 @@ def read_resume_position():
     except FileNotFoundError:
         return 0
     
-# Try to convert pl_pos string to int and check if valid playlist position
+# validate -p input, "res" uses saved resume position, number string is validated as integer
 def validate_pl_pos(pos):
     if pos.lower() == "res":
         resume_pos = read_resume_position()
@@ -78,6 +79,7 @@ def validate_pl_pos(pos):
     except ValueError:
         raise argparse.ArgumentTypeError(f"Invalid playlist position: {pos}.")
 
+# validate -playlist_name input (name without extension)
 def validate_playlist_name(pl_str):
     pattern = re.compile(r'^[a-zA-Z0-9_-]+$')
     if not pattern.match(pl_str):
@@ -85,6 +87,7 @@ def validate_playlist_name(pl_str):
     playlist_name = os.path.join("./Music/", pl_str + ".m3u8")
     return playlist_name
 
+# Check if path in playlist are mp3 files
 def is_mp3_file(file_path):
     return file_path.lower().endswith(".mp3")
 
@@ -103,6 +106,7 @@ def read_playlist(playlist_file):
     except Exception as e:
         print(f'An error occurred: {e}')
 
+# when "-l" try to load path, title, artist, length
 def load_p_t_a_l():
     try:
         with open('path.pkl', 'rb') as file:
@@ -126,14 +130,13 @@ def load_p_t_a_l():
         raise FileNotFoundError("Playlist cannot be loaded, run without '-l'")
     return path, title, artist, length
 
+# generate title, artist, length according to path (music file)
 def create_t_a_l(path):
     title = []
     artist = []
     length = []
-    
     for file_path in path:
         audio = MP3(file_path)
-    
         if 'TIT2' in audio and 'TPE1' in audio:
             t = audio['TIT2'][0]
             a = audio['TPE1'][0]
@@ -142,20 +145,19 @@ def create_t_a_l(path):
             a = ""
         rounded_length = ceil(audio.info.length)
         l = timedelta(seconds=rounded_length)
-
         title.append(t)
         artist.append(a)
         length.append(l)
     
     return title, artist, length
-    
+
+# load relevant lists, decide if saved ones are loaded or generated anew
 def load_playlist(l, playlist_name):
     if l:
         try:
             # pickle load path, title, artist, length
             path, title, artist, length = load_p_t_a_l()
             return path, title, artist, length
-        
         except:
             path = read_playlist(playlist_name)
             #create variables title, artist, length
@@ -167,6 +169,7 @@ def load_playlist(l, playlist_name):
         title, artist, length = create_t_a_l(path)
         return path, title, artist, length
 
+# when "-l" load saved loading, start and end times
 def load_lts_sts_et():
     try:
         with open('lts.pkl', 'rb') as file:
@@ -185,6 +188,7 @@ def load_lts_sts_et():
         raise FileNotFoundError("Playlist cannot be loaded, run without '-l'")
     return lts, sts, et
 
+# generate loading, start and end times for playlist
 def create_lts_sts_et(length, pl_start, pl_len):
     lts = []
     sts = []
@@ -202,12 +206,12 @@ def create_lts_sts_et(length, pl_start, pl_len):
     et = sts[-1] + length[-1]
     return lts, sts, et
 
+# decide if loading, start and end times needs to be loaded "-l" or generated
 def load_runtime_matrix(l, length, pl_start, pl_len):
     if l:
         try:
             lts, sts, et = load_lts_sts_et()
             return lts, sts, et
-        
         except:
             lts, sts, et = create_lts_sts_et(length, pl_start, pl_len)
             return lts, sts, et
@@ -217,22 +221,18 @@ def load_runtime_matrix(l, length, pl_start, pl_len):
 
 # Load a music file with pygame.mixer.music
 def load_music(path, title, artist, pos):
-    #global sound
-    #sound = mixer.Sound(path)
     mixer.music.load(path)
     print(f"Playing {pos}: {title} - {artist}")
-    #mixer.music.set_volume(0.9)
 
 # Start playback of music from RAM memory
 def play_music():
-    #sound.play()
     mixer.music.play()
-    print(f"At: {datetime.now()}")
     while mixer.get_busy() == True:
         continue
     global pl_pos
     pl_pos += 1
 
+# save path, title, artist, length lists to file
 def save_p_t_a_l(path, title, artist, length ):
     with open('path.pkl', 'wb') as file:
         pickle.dump(path, file)
@@ -243,10 +243,12 @@ def save_p_t_a_l(path, title, artist, length ):
     with open('length.pkl', 'wb') as file:
         pickle.dump(length, file)
 
+# save resume position to file
 def save_resume_pos(resume_pos):
     with open('resume_pos.pkl', 'wb') as file:
         pickle.dump(resume_pos, file)
 
+# save loading, start and end times to file
 def save_lts_sts_et(lts, sts, et):
     with open('lts.pkl', 'wb') as file:
         pickle.dump(lts, file)
@@ -255,6 +257,7 @@ def save_lts_sts_et(lts, sts, et):
     with open('et.pkl', 'wb') as file:
         pickle.dump(et, file)
 
+# task cleaning up, closing and quitting script
 def end(path, title, artist, length, lts, sts, et):
         print("Playlist finished playing.")
         # pickle save path, title, artist, length
@@ -299,6 +302,7 @@ if __name__ == "__main__":
     path, title, artist, length = load_playlist(l, playlist_name)
     pl_len = len(path)
     
+    # check for valid playlist position from resume or input
     if 0 <= pl_pos < pl_len:
         pl_start = int(pl_pos)
     else:
@@ -310,23 +314,16 @@ if __name__ == "__main__":
     print(f"Starting with track: {pl_start} , at: {sched_time}")
     
     scheduler = BlockingScheduler(timezone=timezone) # Create a scheduler
+    # sched_time representing first load time (1 second in advance)
     sched_time = sched_time - timedelta(seconds=1)
- 
+    
+    # for the range from the starting track till the end of the playlist generate the actual times for each item and schedule tasks
     for pos in range(pl_start, pl_len):
         p = path[pos]
         t = title [pos]
         a = artist [pos]
-        
         load_time = sched_time + lts[pos]
-        start_time = sched_time + sts[pos]
-        '''
-        # times debugging
-        print(f"lts: {lts[pos]}")
-        print(f"load_time: {load_time}")
-        print(f"sts: {sts[pos]}")
-        print(f"start_time: {start_time}")
-        print(f"length: {length[pos]}")
-        '''        
+        start_time = sched_time + sts[pos]     
         scheduler.add_job(load_music, 'date', run_date=load_time, args=[p,t,a,pos])
         scheduler.add_job(play_music, 'date', run_date=start_time)
 
@@ -335,13 +332,14 @@ if __name__ == "__main__":
     scheduler.add_job(end, 'date', run_date=end_time, args=[path, title, artist, length, lts, sts, et])
     
     try:
-        mixer.init(48000, -16, 1, 1024)
+        mixer.init(48000, -16, 1, 1024) # buffer=1024
         scheduler.start()
-    except (KeyboardInterrupt, SystemExit):
+    except (KeyboardInterrupt, SystemExit): # Interrupt, also activated via ssh control script
         # pickle save path, title, artist, length
         save_p_t_a_l(path, title, artist, length)
         # pickle save pl_pos
         save_resume_pos(resume_pos)
+        # pickle save loading, start and end times
         save_lts_sts_et(lts, sts, et)
         print("Script interrupted by user.")
         scheduler.shutdown(wait=False)
